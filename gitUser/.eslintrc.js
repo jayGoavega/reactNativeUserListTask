2d691cbfa9adfac81db0f8ed2f68425157640742
jayGoavega <jayaprakash.p@goavega.com>
2020-12-16 16:22:21 +0530

    initialCommit

---
+++ gitUser/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+};
---
+++ gitUser/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import UserList from './src/components/userList';
+const App = ():JSX.Element => {
+  return (
+      <UserList />
+  );
+};
+
+export default App;
